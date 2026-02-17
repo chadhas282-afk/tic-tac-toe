38d18252b9660a0969083c19c912766f7e095589
chadhas282-afk <chadhas282@gmail.com>
2026-02-17 19:17:25 +0530

    add logic to game

--- script.js
+++ script.js
@@ -10,4 +10,6 @@ let board = Array(9).fill("")
 let current = "X"
 let active = true
 let score = {X: 0, O:0, T:0}
-
+const wins = [[0,1,2],[3,4,5],[6,7,8],
+      [0,3,6],[1,4,7],[2,5,8],
+      [0,4,8],[2,4,6]]
