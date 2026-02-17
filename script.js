8eb2b31b0d2ff1ba99d5a27c8a24898ee6db8274
chadhas282-afk <chadhas282@gmail.com>
2026-02-17 19:19:17 +0530

    Updates: 2026-02-17T13:49:15.117Z

--- script.js
+++ script.js
@@ -15,5 +15,7 @@ const wins = [[0,1,2],[3,4,5],[6,7,8],
       [0,4,8],[2,4,6]]
 function createBoard(){
     boardEl.innerHTML= ""
-    for(let i=0, i<9, )
+    for(let i=0;i<9;i++){
+        
+    }
 }
