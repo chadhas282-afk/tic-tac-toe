ca7f18220b8a2c855655b52dd6d3b8993d59d404
chadhas282-afk <chadhas282@gmail.com>
2026-02-17 19:38:20 +0530

    add final logic to game

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ const boardEl = document.getElementById('board');
     let active = true;
     let scores = { X: 0, O: 0, T: 0 };
 
-    const wins = [
-      d
-    ];
+ const wins = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+  [0, 4, 8], [2, 4, 6]             
+];
 
     function createBoard(){
       boardEl.innerHTML = '';
